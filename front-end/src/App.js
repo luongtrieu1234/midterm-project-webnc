0d4ad5bbf5495036b535464a5e4c9337dc849941
luongtrieu1234 <luongtrieu1234a@gmail.com>
2023-11-05 22:40:41 +0700

    setup page and route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { SignIn, HomePage, LandingPage, SignUp } from './pages';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" exact element={<LandingPage />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/home-page" element={<HomePage />} />
+    </Routes>
   );
 }
 
