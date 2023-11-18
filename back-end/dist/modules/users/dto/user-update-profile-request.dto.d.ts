0ec29a50c6821063d57ca13a15b6a33968ae0680
luongtrieu1234 <luongtrieu1234a@gmail.com>
2023-11-18 14:48:50 +0700

    add signin

--- back-end/dist/modules/users/dto/user-update-profile-request.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class UserUpdateProfileRequestDto {
-    readonly fullname: string;
-    readonly gender: string;
-    readonly dob: Date;
-    readonly phone: string;
-    readonly address: string;
-    readonly job: string;
-    readonly hobby: string[];
-}
