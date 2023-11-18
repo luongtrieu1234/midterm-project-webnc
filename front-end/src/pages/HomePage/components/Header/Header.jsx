291ea714157f48653ea94c955581d759fbb94200
luongtrieu1234 <luongtrieu1234a@gmail.com>
2023-11-19 01:31:37 +0700

    UI home-page (continute )

---
+++ front-end/src/pages/HomePage/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Button } from 'primereact/button';
+import 'primeicons/primeicons.css';
+import { Link } from 'react-router-dom';
+
+import './Header.css';
+
+const Header = () => {
+  // Logic
+
+  // End Logic
+
+  return (
+    <div>
+        Header
+    </div>
+
+  );
+};
+
+export default Header;
---
+++ front-end/src/pages/HomePage/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Main from "./Main";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { Header, Sidebar, Main };
