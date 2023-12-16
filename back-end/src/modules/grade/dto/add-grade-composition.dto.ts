ccd5c8c1311ebcd8a205b5ef0c3471790123ee67
lexuantien07 <lexuantien07@gmail.com>
2023-12-16 15:19:11 +0700

    grade task (not done)

---
+++ back-end/src/modules/grade/dto/add-grade-composition.dto.ts
@@ -0,0 +1,17 @@
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+export class AddGradeCompositionDto {
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  readonly gradeStructureId: string;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  readonly name: string;
+
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  readonly gradeScale: number;
+}
--- back-end/src/modules/grade/grade.service.ts
+++ back-end/src/modules/grade/grade.service.ts
@@ -1,9 +1,8 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger, Query } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
 import * as bcrypt from 'bcrypt';
-import { JwtService } from '@nestjs/jwt';
 import { AuthService } from '../../others/auth/auth.service';
 import {
   BadRequestException,
@@ -18,16 +17,106 @@ import {
 import { Response, Request } from 'express';
 import { MailService } from '../../others/mail/mail.service';
 import { SharedService } from 'src/others/auth/shared.service';
+import { AddGradeCompositionDto } from './dto/add-grade-composition.dto';
+import { UpdateGradeCompositionDto } from './dto/update-grade-composition.dto';
+import { UserModel } from '../users/users.model';
+import { ClassModel } from '../class/class.model';
+import { GradeCompositionModel } from './grade-composition.model';
+import { GradeStructureModel } from './grade-structure.model';
 
 @Injectable()
 export class GradeService {
   constructor(
     // @Inject('UserRepository')
     // private readonly userRepository: typeof User,
-    private jwtService: JwtService,
     private authService: AuthService,
     private mailService: MailService,
-    private sharedService: SharedService, // @InjectModel('User')
+    private sharedService: SharedService,
+    @InjectModel('User')
+    private readonly userModel: Model<UserModel>,
+    @InjectModel('Class')
+    private readonly classModel: Model<ClassModel>,
+    @InjectModel('GradeComposition')
+    private readonly gradeCompositionModel: Model<GradeCompositionModel>,
+    @InjectModel('GradeStructure')
+    private readonly gradeStructureModel: Model<GradeStructureModel>,
+     // @InjectModel('User')
   ) // private readonly userModel: Model<UserModel>,
   {}
+  async showGradeStructure(classId: string) {
+    try {
+      const classDocument = await this.classModel
+        .findById(classId)
+        .populate('gradeStructure')
+        .exec();
+
+      if (!classDocument) {
+        throw new BadRequestException('Class not found');
+      }
+      if (classDocument.gradeStructure) {
+        const gradeStructureDocument = await this.gradeStructureModel.findById(classDocument.gradeStructure)
+          .populate('gradeComposition')
+          .exec();
+      
+        console.log('gradeStructureDocument', gradeStructureDocument);
+        if (gradeStructureDocument.gradeComposition) {
+          // const gradeCompositionDocument = await GradeCompositionModel.findById(gradeStructureDocument.gradeComposition)
+          //   .populate('grades')
+          //   .exec();
+      
+          // console.log('gradeCompositionDocument ', gradeCompositionDocument);
+          return gradeStructureDocument;
+        }
+      }
+    } catch (error) {
+      console.error('Error retrieving current grade structure:', error.message);
+      throw error;
+    }
+  }
+
+  async addGradeComposition(addGradeCompositionDto: AddGradeCompositionDto) {
+    try {
+      const gradeStructureDocument = await this.gradeStructureModel.findById(addGradeCompositionDto.gradeStructureId);
+      const gradeComposition = await this.gradeCompositionModel.create({
+        name: addGradeCompositionDto.name,
+        gradeScale: addGradeCompositionDto.gradeScale,
+        gradeStructure: addGradeCompositionDto.gradeStructureId,
+      });
+
+      gradeStructureDocument.gradeComposition.push(gradeComposition._id.toString());
+      gradeStructureDocument.save();
+
+      return gradeComposition;
+    } catch (error) {
+      console.error('Error adding grade composition:', error.message);
+      throw error;
+    }
+  }
+
+  async updateGradeComposition(updateGradeCompositionDto: UpdateGradeCompositionDto) {
+    try {
+      const updatedGradeComposition = await this.gradeCompositionModel
+        .findByIdAndUpdate(updateGradeCompositionDto.id, 
+        {
+          name: updateGradeCompositionDto.name,
+          gradeScale: updateGradeCompositionDto.gradeScale,
+        }, 
+        { new: true })
+        .exec();
+
+      return updatedGradeComposition;
+    } catch (error) {
+      console.error('Error updating grade composition:', error.message);
+      throw error;
+    }
+  }
+
+  async removeGradeComposition(gradeCompositionId: string) {
+    try {
+      await this.gradeCompositionModel.findByIdAndDelete(gradeCompositionId).exec();
+    } catch (error) {
+      console.error('Error removing grade composition:', error.message);
+      throw error;
+    }
+  }
 }
